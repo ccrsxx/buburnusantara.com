987502a9b2a6921d69b445b2f5a7dee2ea25fdaa
ccrsxx <aminrisal@gmail.com>
2022-11-13 21:55:07 +0700

    initial commit

---
+++ src/main.ts
@@ -0,0 +1,9 @@
+import './style.scss';
+
+const root = document.getElementById('app') as HTMLDivElement;
+
+root.innerHTML = `
+  <main class='grid place-items-center min-h-screen'>
+    <h1>Hello World</h1>
+  </main>
+`;
---
+++ vite.config.js
@@ -0,0 +1,7 @@
+/** @type {import('vite').UserConfig} */
+export default {
+  base: '/restaurant-website/',
+  server: {
+    port: 80
+  }
+};
