c8696a80ecb3f4adda272efd63f903b677cae72b
ccrsxx <aminrisal@gmail.com>
2022-11-28 22:38:00 +0700

    add color variable

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,12 @@ module.exports = {
   content: ['./index.html', './src/**/*.{html,ts}'],
   theme: {
     extend: {
+      colors: {
+        'main-primary': '#B8B8B8',
+        'main-secondary': '#D4D4D4',
+        'main-background': '#F8F9FA',
+        'accent-orange': '#FDA403'
+      },
       fontFamily: {
         raleway: ['Raleway', 'sans-serif'],
         poppins: ['Poppins', 'sans-serif']
