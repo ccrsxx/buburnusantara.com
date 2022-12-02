2b614901186cfc39fe2d363c76a5e10e8c9a4149
unknown <joywinata88@gmail.com>
2022-12-02 22:45:28 +0700

    add topping

--- src/components/menu.ts
+++ src/components/menu.ts
@@ -31,25 +31,73 @@ const allMenus: Readonly<Category[]> = [
     category: 'topping',
     products: [
       {
-        name: 'Salted Fried Chicken',
-        price: 420_000,
+        name: 'Telur Asin',
+        price: 8_000,
         description:
-          'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequuntur ut necessitatibus obcaecati nobis suscipitiste, blanditiis quam explicabo doloremque eveniet.',
-        image: '/assets/menu/breakfast/breakfast-1.webp'
+          'Telur Bebek yang dipilih dengan hati dan diasinkan dengan rasa yang pas menghasilkan telur asin yang berkualitas tinggi',
+        image: '/assets/menu/topping/topping-1.jpg'
       },
       {
-        name: 'Italian Sauce Mushroom',
-        price: 320_000,
+        name: 'Telur Ayam Kampung',
+        price: 8_000,
         description:
-          'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequuntur ut necessitatibus obcaecati nobis suscipitiste, blanditiis quam explicabo doloremque eveniet.',
-        image: '/assets/menu/breakfast/breakfast-2.webp'
+          'Telur Ayam Kampung yang dipilih dengan hati, sehingga enak disantap mentah maupun matang',
+        image: '/assets/menu/topping/topping-2.jpg'
       },
       {
-        name: 'Fried Potato with Garlic',
-        price: 150_000,
+        name: 'Ati Ampela',
+        price: 7_000,
         description:
-          'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequuntur ut necessitatibus obcaecati nobis suscipitiste, blanditiis quam explicabo doloremque eveniet.',
-        image: '/assets/menu/breakfast/breakfast-3.webp'
+          'Ati dan Ampela ayam yang di rebus dengan rempah pilihan sehingga sangat pas disantap bersama bubur',
+        image: '/assets/menu/topping/topping-3.jpg'
+      },
+      {
+        name: 'Cakwe',
+        price: 6_000,
+        description:
+          'Cakwe yang di goreng dan diberi bumbu pilihan sehingga beraroma khas yang mengharumkan',
+        image: '/assets/menu/topping/topping-4.jpg'
+      },
+      {
+        name: 'Kulit Ayam Kriuk',
+        price: 6_000,
+        description:
+          'Kulit ayam yang digoreng dengan bumbu dan rempah pilihan sangat nikmat disajikan bersama bubur',
+        image: '/assets/menu/topping/topping-5.jpg'
+      },
+      {
+        name: 'Telur Ayam Rebus',
+        price: 6_000,
+        description:
+          'Telur ayam negeri yang sangat cocok dissantap selagi hangat',
+        image: '/assets/menu/topping/topping-2.jpg'
+      },
+      {
+        name: 'Xtra Ayam Kampung',
+        price: 15_000,
+        description:
+          'Ayam kampung yang dibumbui dengan bumbu khas yang mempunyai pilihan kecap maupun biasa',
+        image: '/assets/menu/topping/topping-6.jpg'
+      },
+      {
+        name: 'Krupuk Rengginang',
+        price: 15_000,
+        description:
+          'Krupuk asli Bandung yang digoreng dengan suhu yang pas menghasilkan krupuk yang gurih dan renyah',
+        image: '/assets/menu/topping/topping-7.jpg'
+      },
+      {
+        name: 'Krupuk Ikan',
+        price: 15_000,
+        description:
+          'Krupuk yang dibuat dengan ikan asli menghasilkan krupuk yang renyah dan beraroma ikan yang enak',
+        image: '/assets/menu/topping/topping-8.jpg'
+      },
+      {
+        name: 'Krupuk Jengkol',
+        price: 20_000,
+        description: 'Krupuk Vegetarian jengkol yang tidak berbau',
+        image: '/assets/menu/topping/topping-9.jpg'
       }
     ]
   },
