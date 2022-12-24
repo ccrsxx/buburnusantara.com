97d411f65f4a2c2771d8ffa949230b98b4180289
ccrsxx <aminrisal@gmail.com>
2022-12-24 21:40:05 +0700

    limit tailwindcss content to only typescript files

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./index.html', './src/**/*.{html,ts}'],
+  content: ['./src/**/*.ts'],
   theme: {
     extend: {
       colors: {
